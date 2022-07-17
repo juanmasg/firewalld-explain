5c8460f5ecafe3281107ff30fbdf0257ad905e74
Juanma Sanchez <juasanch@redhat.com>
2022-07-17 10:27:12 +0200

    Support both runtime and sosreport

--- firewalld-explain.py
+++ firewalld-explain.py
@@ -3,23 +3,10 @@
 import sys
 import os
 import re
+from argparse import ArgumentParser
+from subprocess import Popen, PIPE, check_output
 
-class Binds:
-    bindtype = ""
-    zone = ""
-    permits = []
-
-class SourceBind:
-    def __init__(self):
-        self.bindtype = "source"
-
-
-class InterfaceBind:
-    def __init__(self):
-        self.bindtype = "interface"
-
-
-class ZonePermits:
+class Zone:
     name = ""
     ports = []
     protocols = []
@@ -46,114 +33,167 @@ class ZonePermits:
         return getaddr(self, key)
 
     def __repr__(self):
-        return "\n".join([ x for x in (
-                f"{self.name} ({self.target})",
-                f"  Services: {', '.join(self.services)}" if self.services else "",
-                f"  Ports: {', '.join(self.ports)}" if self.ports else "",
-                f"  Source ports: {', '.join(self.source_ports)}" if self.source_ports else "",
-                f"  Protocols: {', '.join(self.protocols)}" if self.protocols else "",
+        return " ".join([ x for x in (
+                f"{self.name} (target:{self.target})",
+                f"  Services: {','.join(self.services)}" if self.services else "",
+                f"  Ports: {','.join(self.ports)}" if self.ports else "",
+                f"  Source ports: {','.join(self.source_ports)}" if self.source_ports else "",
+                f"  Protocols: {','.join(self.protocols)}" if self.protocols else "",
         ) if x])
+
+class Firewalld:
+    _firewallcmd_bin = "firewall-cmd"
+    _zones = None
+    _sources = None
+    _interfaces = None
+
+    def __init__(self):
+        self._zones = {}
+        self._sources = {}
+        self._interfaces = {}
+
+    def _parse_all_zones(self, contents):
+
+        last_key = ""
+        current_zone_active = False
+
+        for line in contents.split("\n"):
+            if not line:
+                # ^$: end of zone
+                last_key = ""
+                continue
+
+            elif re.match("\w", line):
+                # new zone
+
+                words = line.split(" ")
+                if len(words) == 1: 
+                    # zone not (active)
+                    current_zone_active = False
+                    continue
+
+                current_zone_active = True
+                current_zone_name = words[0]
+                self._zones[current_zone_name] = Zone(name=current_zone_name)
+
+            elif last_key == "rich rules":
+                # ignore rich rules for now
+                continue
+
+            elif not current_zone_active:
+                # ignore data from inactive zones
+                continue
+
+            else:
+                key, values = [ x.strip() for x in line.split(":", 1) ]
+                last_key = key
+
+                if not values:
+                    continue
+
+                values = [x.strip() for x in values.split(" ") if x.strip() ]
+
+                if key == "sources":
+                    for source in values:
+                        self._sources[source] = self._zones[current_zone_name]
+                elif key == "interfaces":
+                    for interface in values:
+                        self._interfaces[interface] = self._zones[current_zone_name]
+
+                elif key in ("source-ports", "ports", "services", "protocols"):
+                        key = key.replace("-", "_")
+                        for value in values:
+                            self._zones[current_zone_name][key] = values
+
+                elif key == "target":
+                    self._zones[current_zone_name][key] = values[0]
+
+
+
+    def list_all_zones(self):
+        cmd = f"{self._firewallcmd_bin} --list-all-zones"
+        proc = Popen(cmd.split(" "), stdout=PIPE)
+        stdout, stderr = proc.communicate()
+        if stderr and b"FirewallD is not running" in stderr:
+            print(stderr)
+
+        return stdout.decode("utf8")
+
+    def _parse_firewalld_conf(self, contents):
+        pairs = [ x.split("=") for x in contents.split('\n') \
+                if x and not x.startswith("#") ]
+
+        return dict(pairs)
+
+    def firewalld_conf(self):
+        filepath = "/etc/firewalld/firewalld.conf"
+        contents = open(filepath).read()
+        return self._parse_firewalld_conf(contents)
+
+    def explain(self):
+        contents = self.list_all_zones()
+        self._parse_all_zones(contents)
+
+        for source, zone in self._sources.items():
+            print(f"{source} -> {zone}")
+        
+        for interface, zone in self._interfaces.items():
+            print(f"{interface} -> {zone}")
+
+        firewalld_conf = self.firewalld_conf()
+        
+        default_zone_name = firewalld_conf.get("DefaultZone")
+        if default_zone_name:
+            default_zone = zones.get(default_zone_name)
+        print(f"All_other_traffic -> {default_zone}")
+
+
+class SOSFirewalld(Firewalld):
+    _sospath = None
+
+    def __init__(self, sospath):
+        super().__init__(self)
+        self._sospath = sospath
+
+    @staticmethod
+    def check_sos_path(sospath):
+        if not os.path.isdir(sospath):
+            return False
+
+        if not os.path.isdir(f"{sospath}/sos_commands/firewalld"):
+            return False
+
+        return True
+
+
+    def list_all_zones(self):
+        filepath = f"{self._sospath}/sos_commands/firewalld/firewall-cmd_--list-all-zones"
+        return open(filepath).read()
+
+    def firewalld_conf(self):
+        filepath = f"{self._sospath}/etc/firewalld/firewalld.conf"
+        contents = open(filepath).read()
+        return self._parse_firewalld_conf(contents)
+
 
 sources = {}
 interfaces = {}
 zones = {}
 
-sosreport=sys.argv[1]
-firewalld_dir=f"{sosreport}/sos_commands/firewalld"
-firewalld_list_all_zones=f"{firewalld_dir}/firewall-cmd_--list-all-zones"
-firewalld_conf=f"{sosreport}/etc/firewalld/firewalld.conf"
+parser = ArgumentParser()
+parser.add_argument("--sos", "-S", help="Path to sosreport with firewalld dump")
 
-if not os.path.isdir(sosreport):
-    print(f"Sosreport path doesn't exist {sosreport}")
-    sys.exit(1)
+args = parser.parse_args()
 
-if not os.path.isdir(firewalld_dir):
-    print(f"Sosreport doesn't contain a firewalld dump {sosreport}")
-    sys.exit(1)
+firewalld = None
 
-if not os.path.exists(firewalld_list_all_zones):
-    print(f"Sosreport firewalld doesn't include a zone dump {sosreport}")
-    sys.exit(1)
+if args.sos:
+    if not SOSFirewalld.check_sos_path(args.sos):
+        printf("sosreport path doesn't seem to contain firewalld data {args.sos}")
+        sys.exit(1)
 
+    firewalld = SOSFirewalld(args.sos)
+else:
+    firewalld = Firewalld()
 
-firewalld_conf_params = dict([ x.split("=") for x in open(firewalld_conf).read().split('\n') if x and not x.startswith("#") ])
-
-explained = {}
-current_zone=None
-current_zone_active=False
-current_triggers = []
-current_permits = []
-last_key = ""
-
-for line in open(firewalld_list_all_zones).read().split('\n'):
-    #print("LINE", line, "  --  ", f"LAST KEY \"{last_key}\"")
-
-    if not line: # ^$: end of zone
-        last_key = ""
-        continue
-
-    #if not line.startswith(" "):
-    if re.match("[a-zA-Z0-9]", line):  #^zonename (active)?
-        # New Zone
-
-        words = line.split(" ")
-        if len(words) == 1:
-            current_zone_active=False
-            continue
-
-        current_zone_active=True
-        zone_name = words[0]
-        current_zone = zone_name
-
-        zones[zone_name] = ZonePermits(name=zone_name)
-
-        #print(f"Zone {zone_name}: {current_zone_active}")
-
-    elif last_key == "rich rules":
-        # Ignore rich rules for now
-        continue
-
-    elif not current_zone_active:
-        # Ignore keys from inactive zones
-        continue
-
-    else:
-        key, values = [ x.strip() for x in line.split(":", 1) ]
-        last_key = key
-
-        if not values:
-            continue
-
-        values = [x.strip() for x in values.split(" ") if x.strip() ]
-
-        if key == "sources":
-            for source in values:
-                sources[source] = zones[zone_name]
-        elif key == "interfaces":
-            for interface in values:
-                interfaces[interface] = zones[zone_name]
-
-        elif key in ("source-ports", "ports", "services", "protocols"):
-                key = key.replace("-", "_")
-                for value in values:
-                    zones[zone_name][key] = values
-
-        elif key == "target":
-            zones[zone_name][key] = values[0]
-
-        #print("\tKEY", key, "VALUE", values)
-
-for source, zone in sources.items():
-    print(f"{source} -> {zone}\n")
-
-for interface, zone in interfaces.items():
-    print(f"{interface} -> {zone}\n")
-
-default_zone_name = firewalld_conf_params.get("DefaultZone")
-if default_zone_name:
-    default_zone = zones.get(default_zone_name)
-print(f"All other traffic -> {default_zone}")
-
-
-#print("SOURCES", sources)
-#print("INTERFACES", interfaces)
+firewalld.explain()
